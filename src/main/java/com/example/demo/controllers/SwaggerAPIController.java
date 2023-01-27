c53354b511b644c3d117231f931a183ff6afb221
Asim Azizov <asimazizov162003@gmail.com>
2023-01-27 09:21:25 +0300

    Last changes

--- src/main/java/com/example/demo/controllers/SwaggerAPIController.java
+++ src/main/java/com/example/demo/controllers/SwaggerAPIController.java
@@ -1,17 +1,22 @@
-package com.example.demo;
+package com.example.demo.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
+@RestController
+public class SwaggerAPIController {
 
-@Controller
-public class GreetingController {
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+    @GetMapping("/Hello")
+    public String homePage(){
+        return "Hello...";
+    }
+
+
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
 class DemoApplicationTests {
 
 	@Test
