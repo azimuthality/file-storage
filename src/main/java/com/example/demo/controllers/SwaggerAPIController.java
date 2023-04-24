85eb7ea5b12a3d18e8330e4d0354c86a141586c7
azimuthality <azizov.asimkpc322020@mail.ru>
2023-04-24 15:59:06 +0300

    feat: add controller WIP

--- src/main/java/com/example/demo/controllers/SwaggerAPIController.java
+++ src/main/java/com/example/demo/controllers/SwaggerAPIController.java
@@ -1,16 +1,42 @@
 package com.example.demo.controllers;
 
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/add")
 public class SwaggerAPIController {
 
-    @RequestMapping( value = "/file-link", method = GET)
-    public String fileLink(){
-        return "Get";
+
+    @GetMapping( value = "/get-image-with-media-type",
+        produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody byte[] getImage() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/com/example/demo/files/izo.jpg");
+        return IOUtils.toByteArray(in);
     }
+    @GetMapping(
+            value = "/get-text",
+            produces = MediaType.TEXT_HTML_VALUE
+    )
+    public @ResponseBody byte[] getFile() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/com/example/demo/files/text.txt");
+        return IOUtils.toByteArray(in);
+    }
+
+
+
+
     @RequestMapping(value = "/list-file", method = GET)
     public String listFile(){
         return "list-file";
@@ -20,6 +46,7 @@ public class SwaggerAPIController {
     public String postFos() {
         return "Post";
     }
+
     @RequestMapping(value = "/delete", method = DELETE)
     public String deleteFos(){
         return "Delete";
